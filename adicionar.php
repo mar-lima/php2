90cd14a9153c45439cc8b33d41497ffc03dad7e2
mar-lima <marcelo.llima135@gmail.com>
2022-03-23 19:00:05 -0300

    master

---
+++ adicionar.php
@@ -0,0 +1,20 @@
+<h1>Adiconar usuario</h1>
+<form method="post" action="adicionar_action.php">
+    <label for="">
+        Nome <br>
+        <input type="text" name="name">
+    </label> <br><br>
+    <label for="">
+        E-mail <br>
+        <input type="email" name="email">
+    </label> <br><br>
+
+    <input type="submit" value="Adicionar">
+
+</form>
+
+
+<?php
+
+
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+<body>
+    
+    <a href="adicionar.php">ADICIONAR USUARIO</a>
+
+    <table border="1" width='100%'>
+
+        <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>EMAIL</th>
+            <th>AÇÕES</th>
+        </tr>
+
+    </table>
+
+</body>
+</html>
+
+
+
+
